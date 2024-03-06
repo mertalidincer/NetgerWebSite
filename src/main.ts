18761d25ad1738a3b1d355f25b884029a90662b5
apokral5 <haktanabdullah123@gmail.com>
2024-03-06 23:13:13 +0300

    büyük resim
    netger büyük resim eklenmesi

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+import { error } from 'node:console';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
